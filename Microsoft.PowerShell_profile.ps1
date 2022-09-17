439b8e7852bf47e703ba30d443dc44c0975f1e53
nobushi95 <nobushi1121@gmail.com>
2022-09-17 16:08:29 +0900

    powershell profile を追加

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,48 @@
+# powershell でのシンボリックリンクの作り方
+# New-Item -Type SymbolicLink {sourceFile} -Value {destFile}
+
+Import-Module posh-git
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\jandedobbeleer.omp.json" | Invoke-Expression
+
+Import-Module Terminal-Icons
+
+# Alias
+
+## VSCode
+function code-r { code -r $args }
+Set-Alias -Name cr -Value code-r
+
+## powershell make
+function source-fun { . $PROFILE }
+Set-Alias -Name source-profile -Value source-fun
+
+## git
+Set-Alias -Name g -Value git
+
+function git-pull { git pull $args }
+Set-Alias -Name pull -Value git-pull
+
+function git-push { git push $args }
+Set-Alias -Name push -Value git-push
+
+function git-fetch { git fetch $args }
+Set-Alias -Name fetch -Value git-fetch
+
+function git-add { git add $args }
+Set-Alias -Name ga -Value git-add
+
+function git-branch { git branch $args }
+Set-Alias -Name gb -Value git-branch
+
+function git-diff { git diff $args }
+Set-Alias -Name gd -Value git-diff
+
+function git-status { git status --short $args }
+Set-Alias -Name gs -Value git-status
+
+# gm が Get-Member のデフォルトエイリアスとなっているため不可
+# Set-Alias のオプションで -Option AllScope とすればよさそう
+# function git-commit-with-arguments-message { git commit --message $args }
+# Set-Alias -Name gm -Value git-commit-with-arguments-message
