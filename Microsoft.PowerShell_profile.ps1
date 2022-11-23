c1a1cb417eaa4d2b5d1926b2e49cfaad40b04895
nobushi95 <nobushi1121@gmail.com>
2022-11-23 23:38:03 +0900

    PowerShellにgmエイリアスを作成

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -67,7 +67,8 @@ Set-Alias -Name gd -Value git-diff
 function git-status { git status --short $args }
 Set-Alias -Name gs -Value git-status
 
-# gm が Get-Member のデフォルトエイリアスとなっているため不可
-# Set-Alias のオプションで -Option AllScope とすればよさそう
-# function git-commit-with-arguments-message { git commit --message $args }
-# Set-Alias -Name gm -Value git-commit-with-arguments-message
+# gm が Get-Member のデフォルトエイリアスとなっているため上書き
+# Set-Alias のオプションで -Option AllScope とすればよさそう？？ -> -Forceで上書きも必要？
+# 参考: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/set-alias?view=powershell-7.3#example-3-create-and-change-a-read-only-alias
+function git-commit-with-arguments-message { git commit --message $args }
+Set-Alias -Name gm -Value git-commit-with-arguments-message -Force -Option AllScope
