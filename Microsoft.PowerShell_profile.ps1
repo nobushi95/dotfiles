dda10388132292d1a8741180fc884a98f8aff836
nobushi95 <nobushi1121@gmail.com>
2023-01-05 23:18:49 +0900

    posh-gitのcompletionがalias設定したgitコマンドでも使えるように変更

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,33 +1,8 @@
 # powershell でのシンボリックリンクの作り方（File・Folder）
 # New-Item -Type SymbolicLink [-name] {dest} -Target {src}
 
-# ビープ音無効化
-Set-PSReadlineOption -BellStyle None
-
-Import-Module posh-git
-Import-Module Terminal-Icons
-
-oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\powerlevel10k_rainbow.omp.json" | Invoke-Expression
-
-# PowerShell7
-if (7 -eq $PSVersionTable.PSVersion.Major) {
-    ## auto suggestion
-    Set-PSReadLineOption -PredictionSource History
-    Set-PSReadLineOption -PredictionViewStyle ListView
-}
-
-# emacs keybinding
-Import-Module PSReadLine
-Set-PSReadlineOption -EditMode Emacs
-Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteChar
-Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
-
-# tab-completions
-(& volta completions powershell) | Out-String | Invoke-Expression
-(& rustup completions powershell) | Out-String | Invoke-Expression
-# (& rustup completions powershell cargo) | Out-String | Invoke-Expression # cargoは未サポート
-
 # Alias
+# gitなどのcompletionが設定したaliasでも動作するように、Import-Moduleの前に記載する
 
 ## base
 Set-Alias l ls
@@ -73,3 +48,30 @@ Set-Alias -Name gs -Value git-status
 # 参考: https://learn.microsoft.com/en-us/powershell/module/microsoft.powershell.utility/set-alias?view=powershell-7.3#example-3-create-and-change-a-read-only-alias
 function git-commit-with-arguments-message { git commit --message "$args" }
 Set-Alias -Name gm -Value git-commit-with-arguments-message -Force -Option AllScope
+
+
+# ビープ音無効化
+Set-PSReadlineOption -BellStyle None
+
+Import-Module posh-git
+Import-Module Terminal-Icons
+
+oh-my-posh init pwsh --config "$(scoop prefix oh-my-posh)\themes\powerlevel10k_rainbow.omp.json" | Invoke-Expression
+
+# PowerShell7
+if (7 -eq $PSVersionTable.PSVersion.Major) {
+    ## auto suggestion
+    Set-PSReadLineOption -PredictionSource History
+    Set-PSReadLineOption -PredictionViewStyle ListView
+}
+
+# emacs keybinding
+Import-Module PSReadLine
+Set-PSReadlineOption -EditMode Emacs
+Set-PSReadlineKeyHandler -Key Ctrl+d -Function DeleteChar
+Set-PSReadLineKeyHandler -Key Tab -Function MenuComplete
+
+# tab-completions
+(& volta completions powershell) | Out-String | Invoke-Expression
+(& rustup completions powershell) | Out-String | Invoke-Expression
+# (& rustup completions powershell cargo) | Out-String | Invoke-Expression # cargoは未サポート
